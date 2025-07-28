81a063f2a1adf77be0dc194f4390681886bf9109
Dilaa13 <charithdhanam@gmail.com>
2025-07-28 22:23:10 +0530

    sample main.c file for the simulation

---
+++ main.c
@@ -0,0 +1,13 @@
+#define F_CPU 16000000UL  // 16 MHz external crystal
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+    DDRB |= (1 << PB0);  // Set PB0 as output
+
+    while (1) {
+        PORTB ^= (1 << PB0);  // Toggle PB0
+        _delay_ms(500);       // 500 ms delay
+    }
+}
